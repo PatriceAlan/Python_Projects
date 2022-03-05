a8e8e752fc98cf8433375dfac84d1c6ce26dbcd1
PatriceAlan <100718438+PatriceAlan@users.noreply.github.com>
2022-03-05 22:57:14 +0100

    Add files via upload

---
+++ Desktop_Notifier.py
@@ -0,0 +1,6 @@
+from plyer import notification
+
+title = "Hey there it's Alan"
+message = "Y'all are amazing !!!"
+
+notification.notify(title=title, message=message, app_icon=None, timeout=60, toast=False)
